ce2d556a1b7c7272876b2112d4e189bcd374fd43
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-03 20:19:34 +0800

    added two scripts

---
+++ variable.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#encoding:utf8
+
+
+v_username = 'alvin'
+
+v_password = 'alvin\'s Password'
+
+print ("My name is {name}, and my password is {pw}".format(name=v_username,pw=v_password))
