cad63136d7634a0a0b54035b62474c74b406ade5
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-04 21:49:07 +0800

    added class.py

---
+++ class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#coding:utf8
+
+class Bird:
+    hungry = 50
+    def sing(self):
+        self.hungry += 10
+    def eat(self):
+        self.hungry -= 10
+
+b = Bird()
+print (b.hungry)
