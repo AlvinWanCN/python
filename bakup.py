cc18eac64b5b3c2ba7eaedbb2322461040b86106
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-12 20:27:03 +0800

    i

--- bakup.py
+++ bakup.py
@@ -6,5 +6,10 @@ v_sorcDir='/home/alvin'
 v_destDir='/tmp/alvin'
 
 
-v_fileName='{s}/dir1,{s}/dir2,,{s}/dir3'.format(s=v_sorcDir)
-print v_fileName.split(',')
+v_fileName='{s}/dir1,{s}/dir2,{s}/dir3'.format(s=v_sorcDir)
+a =  v_fileName.split(',')
+
+for i in a:
+    os.chdir(v_sorcDir)
+    sortName=i.split('/home/alvin/')[1]
+    os.system('tar cf {d}/{s}.tar.gz {s}'.format(d=v_destDir,s=sortName))
