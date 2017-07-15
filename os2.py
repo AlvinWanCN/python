49856385e1f4bb56674c851270a4fcca23c68f2b
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-15 18:12:45 +0800

    ok

--- os2.py
+++ os2.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
+#coding:utf8
+
 import os
 
-var = 'ok'
+#var = 'ok'
+#
+#os.environ['yes']=str(var)
+#os.system('echo $yes')
 
-os.environ['yes']=str(var)
-os.system('echo $yes')
+#a=os.system('ps aux|grep asda|grep ada|grep -v grep')
+#print a
+a = os.system('ls  &>/dev/null')
+print a
