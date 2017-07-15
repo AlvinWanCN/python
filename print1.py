7c87312770d41fe062966fde31435eab4325f9b3
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-15 18:34:53 +0800

    splitline

--- print1.py
+++ print1.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 #coding:utf8
 
+'''
 #用户切分目录与文件名，获取到我们的文件名。
 a='/home/alvin/app/nginx/www/index.html'
 print a.rsplit('/',1)[-1]
+'''
+
+'''
+#以行作为切分
+a="""hello
+this is alvin
+nice to meet you"""
+
+print a.splitlines()
+'''
