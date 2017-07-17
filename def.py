81b79e0f8d37e589af660e8ef243de9528f0fe9a
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-17 21:39:59 +0800

    Update def.py

--- def.py
+++ def.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 #coding:utf8
+'''
+def ap(i,e):
+    print ("first is {i},last is  {nnn}".format(i=i,nnn=e))
 
+bb = 'facebook'
+#ap(1)
+ap('alvin',bb)
+'''
 
 def aa(a,b):
     if a <= 0:
