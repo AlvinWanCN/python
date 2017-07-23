a0e2fb3757949ebee29c9a510dffca877a8c58be
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-23 23:31:39 +0800

    ok

---
+++ test/socketTest/server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #socket type
+        #socket.AF_INET     ipv4协议的socket套接字
+        #socket.AF_INET6    ipv6协议的socket套接字
+        #socket.AF_UNIX     Unix系统间数据进行传输的套接字
+    #socket family
+        #socket.SOCK_STREAM tcp协议的socket
+        #socket.SOCK_DGRAM  udp协议的socket
