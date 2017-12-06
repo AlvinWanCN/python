c111517fa5f3ffca81b9541b8a1a57476b16d93f
alvin <alvin.wan.cn@hotmail.com>
2017-12-06 09:58:36 +0800

    This is Alvin Wan, I commited this change.

--- test/socketTest/clientudp.py
+++ test/socketTest/clientudp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #coding:utf-8
-
+import socket,time
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind(("",8001))
@@ -10,5 +10,7 @@ sock.bind(("",8001))
 sock.sendto("hello world Iam 8001",("127.0.0.1",8000))
 data,address = sock.recvfrom(512)
 #udp 服务器接受recvfrom
-
-print("%s:%s is content"%address)
+print("%s:%s is connect "%address)
+print(data)
+#关闭
+sock.close()
