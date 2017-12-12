de0060c1188764418bcd806d4073bfb4d91e579f
alvin <alvin.wan.cn@hotmail.com>
2017-12-12 15:12:28 +0800

    This is Alvin Wan, I commited this change.

--- common/guess_number.py
+++ common/guess_number.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
 #coding:utf-8
+import random
 
 
-LuckNumber = 234
+LuckNumber = random.randrange(1000)
 
 Congratulation='Congratulation! you are found the right number success! The luck number is %s'
 LessThan='Sorry the number your typed %s is less than luck number,Please try again.'
 MoreThan='Sorry the number your typed %s more than luck number,Please try again.'
 Error='Sorry, Something is wrong, Please try again, or contact admin'
+chance='This is your %s chance.'
+successTimes="You win at %s chance!"
 #print("Your number is %s"%Number)
-
+times=1
 while True:
+    print(chance%times)
     Number = int(raw_input('Please enter a number, figure range in 0-1000:'))
     if Number == LuckNumber:
         print(Congratulation%LuckNumber)
+        print(successTimes%times)
         break
     elif Number > LuckNumber:
         print(MoreThan%Number)
@@ -21,3 +26,4 @@ while True:
         print (LessThan%Number)
     else:
         print(Error)
+    times += 1
---
+++ test/test1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+#coding:utf-8
+import random
+print(random.randrange(90,100))
