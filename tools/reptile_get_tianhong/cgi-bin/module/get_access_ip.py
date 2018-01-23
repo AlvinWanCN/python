1e2b3370b4b6326b9d27c9d9120b892a1f5ecad9
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-23 18:01:32 +0800

    1

--- tools/reptile_get_tianhong/cgi-bin/module/get_access_ip.py
+++ tools/reptile_get_tianhong/cgi-bin/module/get_access_ip.py
@@ -9,4 +9,4 @@ all_content=f1.read()
 class getip():
     def ipinfo(self):
         #print all_content
-        return re.findall(r'\n(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s',all_content)[1] #返回日志文件里最后一个IP
+        return re.findall(r'\n(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s',all_content)[-1] #返回日志文件里最后一个IP
