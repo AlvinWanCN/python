0dd814c7f07616a7b8efcf67b0b311b856813e1b
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-23 13:50:16 +0800

    ok

--- tools/reptile_get_tianhong/get_tianhong.py
+++ tools/reptile_get_tianhong/get_tianhong.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python
 #coding:utf-8
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 import urllib2,re,time
-
+from module.fundDB import fund_tab
 response = urllib2.urlopen("http://www.howbuy.com/fund/ajax/gmfund/valuation/valuationnav.htm?jjdm=000961")
 content= response.read()
 #print content
-latestValue = re.findall(r'con_value con_value_up\">(.*)</',content)
-latestPercent= re.findall(r'con_ratio_red\">(.*)</',content)
+latestValue = re.findall(r'con_value con_value_up\">(.*)</',content)[0]
+latestPercent= re.findall(r'con_ratio_red\">(.*)</',content)[0]
 
-Nowtime=time.strftime('%Y年%m月%d日 %H:%M:%S')
+Nowtime=time.strftime('%Y-%m-%d %H:%M:%S')
 #print(latestValue)
 #print(latestPercent)
-earnings = float(latestValue[0])*63351.09-79930.07
+earnings = float(latestValue)*63351.09-79930.07
 
 
+class usedb():
+    f = fund_tab()
+    def insert_db(self):
+        self.f.value=latestValue
+        self.f.percent=latestPercent
+        self.f.date=time.strftime('%Y-%m-%d %H:%M:%S')
+        self.f.save()
+    def query_db(self):
+        t_list=self.f.select().order_by(fund_tab.date.desc()).limit(1)
+        for T in t_list:
+            global last_value,last_date,last_percent
+            last_value=T.value
+            last_date=T.date
+            last_percent=T.percent
+
+db=usedb()
+db.query_db()
+db.insert_db()
+print(last_value)
 html = """
 
 <html>
@@ -26,16 +47,14 @@ html = """
     </head>
     <body>
         <p>
-        最新天弘天弘沪深300指数估值：%s
-        </p>
-        <p>
-        今日天弘天弘沪深300指数涨幅：%s
+        上次天弘天弘沪深300指数估值：%s ， 涨幅是 %s 查询时间是 %s </br>
+        最新天弘天弘沪深300指数估值：%s ， 涨幅是 %s 当前时间是 %s
         </p>
         <p>
            我的天弘基金收益：￥ %s
         </p>
         <p>
-            Alvin Wan  %s
+            Alvin Wan  
         </p>
     </body>
 </html>
@@ -43,4 +62,4 @@ html = """
 """
 print("Content-type:text/html")
 print()
-print(html%(latestValue[0],latestPercent[0],earnings,Nowtime))
+print(html%(last_value,last_percent,last_date,latestValue,latestPercent,Nowtime,earnings))
