bca7fd7f9cb077481f2a8835fd07a6c90dd5e809
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-23 17:39:42 +0800

    1

--- tools/reptile_get_tianhong/cgi-bin/module/get_access_ip.py
+++ tools/reptile_get_tianhong/cgi-bin/module/get_access_ip.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 #coding:utf-8
-import re,time,os
-#time.sleep(2)
+import re,time
+time.sleep(2)
 logfile='/var/log/nginx/access.log' #设置日志文件路径
-#f1=open(logfile,'r')
-#all_content=f1.read()
+f1=open(logfile,'r')
+all_content=f1.read()
 class getip():
     def ipinfo(self):
-        #return re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',all_content)[-1] #返回日志文件里最后一个IP
-        return re.sub(r'\s+','',os.popen("tail -1 %s |sed 's/ .*//'"%logfile).read())
+        return re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',all_content)[-1] #返回日志文件里最后一个IP
