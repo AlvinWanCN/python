bfcc10f42a5739b070ad291d49c284e3276223b0
alvin <alvin.wan.cn@hotmail.com>
2018-01-24 22:48:24 +0800

    This is Alvin Wan, I commited this change.

--- tools/reptile_get_tianhong/cgi-bin/module/get_access_ip.py
+++ tools/reptile_get_tianhong/cgi-bin/module/get_access_ip.py
@@ -8,14 +8,14 @@ if os.path.exists(log1):
     logfile = log1
 elif os.path.exists(log2):
     logfile = log2
-
-
-
+try:
+    f1 = open(logfile, 'r')
+    all_content = f1.read()
+except:
+    pass
 class getip():
         def ipinfo(self):
             try:
-                f1 = open(logfile, 'r')
-                all_content = f1.read()
                 #print all_content
                 return re.findall(r'\n(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s',all_content)[-1] #返回日志文件里最后一个IP
             except:
