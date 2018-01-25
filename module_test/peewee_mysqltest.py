586e804482f8ea261623935bdfd1ca87db9a1804
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-25 09:36:06 +0800

    PY-2017.1.4 <Alvin Wan@Alvin_Win10 Overwrite remote git@github.com:AlvinWanCN/python.git to local

--- module_test/peewee_mysqltest.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-#coding:utf-8
-from peewee1 import sophia
-aa=sophia.raw('select * from sophiroth')
-print(aa)
--- tools/computer1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#coding:utf-8
-#wait for write.
-
-a = 10
-print a
-a += 10
-print a
