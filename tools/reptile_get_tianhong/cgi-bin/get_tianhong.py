4f535b2b6ee87b2ed01cabe280b2779445ccc742
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-25 10:57:26 +0800

    1

--- tools/reptile_get_tianhong/cgi-bin/get_tianhong.py
+++ tools/reptile_get_tianhong/cgi-bin/get_tianhong.py
@@ -12,7 +12,6 @@ except:
 
 response = urllib.request.urlopen("http://www.howbuy.com/fund/ajax/gmfund/valuation/valuationnav.htm?jjdm=000961")
 content= response.read()
-#print content
 latestValue = re.findall(r'con_value con_value_\w+\">(.*)</',content.decode('utf-8'))[0]
 latestPercent= re.findall(r'con_ratio_\w+\">(.*)</',content.decode('utf-8'))[0]
 
