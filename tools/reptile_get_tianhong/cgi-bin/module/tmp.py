954dcfd20382c9e2becd6fd88decdaf1d2327b6e
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-25 15:47:50 +0800

    I changed again.

--- tools/reptile_get_tianhong/cgi-bin/module/tmp.py
+++ tools/reptile_get_tianhong/cgi-bin/module/tmp.py
@@ -5,8 +5,8 @@ from get_access_ip import getip
 
 gip=getip()
 print (gip.ipinfo())
-thdict={}
-content = urllib.request.urlopen("http://www.howbuy.com/fund/ajax/gmfund/valuation/valuationnav.htm?jjdm=000961").read().decode('utf-8')
+#thdict={}
+#content = urllib.request.urlopen("http://www.howbuy.com/fund/ajax/gmfund/valuation/valuationnav.htm?jjdm=000961").read().decode('utf-8')
 
 #thdict['latestValue'] = re.findall(r'con.*\">(.*)<',content)[0]
-print(type(re.findall(r'con.*\">(.*)<',content)))
+#print(type(re.findall(r'con.*\">(.*)<',content)))
