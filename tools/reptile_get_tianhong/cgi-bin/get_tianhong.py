102a9e94db06765fd2fb037b41f4768231ac5cab
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-30 18:11:09 +0800

    I changed again.

--- tools/reptile_get_tianhong/cgi-bin/get_tianhong.py
+++ tools/reptile_get_tianhong/cgi-bin/get_tianhong.py
@@ -13,13 +13,22 @@ thdict={}
 totalFund=75941.89
 totalMoney=96000
 content = urllib.request.urlopen("http://www.howbuy.com/fund/ajax/gmfund/valuation/valuationnav.htm?jjdm=000961").read().decode('utf-8')
-#thdict['latestValue'] = re.findall(r'con.*\">(.*)<',content)[0]
-#thdict['latestBenefit']= re.findall(r'con.*\">(.*)<',content)[1]
-#thdict['latestPercent']=re.findall(r'con.*\">(.*)<',content)[2]
+'''直接从原文匹配,相似内容比较少的情下比较方便使用这种方法，这里我们注释掉了，不用这种匹配方式
+thdict['latestValue'] = re.findall(r'con.*\">(.*)<',content)[0] 
+thdict['latestBenefit']= re.findall(r'con.*\">(.*)<',content)[1]
+thdict['latestPercent']=re.findall(r'con.*\">(.*)<',content)[2]
+'''
+'''通过bs来匹配，找到指定类型的标签。找这里我们也注释掉了，不用这种匹配方式
 content_list=BS(content,'lxml').find_all('span')
-thdict['latestValue'] =re.findall(r'>(.*)<',str(content_list[0]))[0]
+thdict['latestValue'] =re.findall(r'>(.*)<',str(content_list[0]))[0]  #通过BS查找指定属性后匹配
 thdict['latestBenefit']= re.findall(r'>(.*)<',str(content_list[1]))[0]
 thdict['latestPercent']=re.findall(r'>(.*)<',str(content_list[2]))[0]
+'''
+from lxml import etree #这里我们使用lxml 的etree，通过直接找xpath来找到内容
+html=etree.HTML(content)
+thdict['latestValue'] =html.xpath('/html/body/li/span[1]')[0].text #获取xpath的方法，可以谷歌浏览器里访问网站后，按F12，在Elements里找到我们的指定内容，然后右键——Copy——Copy XPath，然后把Xpath地址贴到这里来
+thdict['latestBenefit']= html.xpath('/html/body/li/span[2]')[0].text #这里我们找的是列表里的text，除了text外，如果需要还可以获取tag和attrib
+thdict['latestPercent']=html.xpath('/html/body/li/span[3]')[0].text
 thdict['Nowtime']=time.strftime('%Y-%m-%d %H:%M:%S')
 thdict['date']=re.sub(r'\s','%20',time.strftime('%Y-%m-%d %H:%M:%S'))
 thdict['earnings'] = '%.2f' % float(float(thdict['latestValue'])*totalFund-totalMoney)
---
+++ tools/reptile_get_tianhong/cgi-bin/module/chicang.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#coding:utf-8
+import urllib.request,re
+from  bs4 import BeautifulSoup as BS
+from lxml import etree
+baseurl="http://finance.sina.com.cn/fund/quotesold/000961/bc.shtml"
+#baseurl='http://www.howbuy.com/fund/ajax/gmfund/valuation/valuationnav.htm?jjdm=000961'
+content = urllib.request.urlopen(baseurl).read().decode('gbk')
+#print(content)
+#content_list=BS(content,'lxml').find_all('td')
+#print(len(content_list))
+#print(content_list)
+
+html=etree.HTML(content)
+content_list=html.xpath('//*[@id="blue-chips-sh601318"]/td[1]/a')
+content_list=html.xpath('//*[@id="blue-chips-sh601318"]/td[3]/span')
+#for i in content_list:
+print(len(content_list))
+print(float(content_list[0].text))
+print(content_list[0].tag)
+print(content_list[0].attrib)
