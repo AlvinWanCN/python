20f9f7f2a5bb9e987bc58da9be3af83824b7b273
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-01-31 11:27:00 +0800

    I changed again.

---
+++ test/apis/sms1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#coding:utf-8
+import urllib.request, sys
+import ssl
+
+
+host = 'https://fesms.market.alicloudapi.com'
+path = '/sms/'
+method = 'GET'
+appcode = '4ca26b23675f4b7f81b365d115184cdf'
+querys = 'code=895642&phone=10000000000&skin=12'
+bodys = {}
+url = host + path + '?' + querys
+
+request = urllib.request.Request(url)
+request.add_header('Authorization', 'APPCODE ' + appcode)
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+response = urllib.request.urlopen(request, context=ctx)
+content = response.read()
+if (content):
+    print(content)
--- test/t1.py
+++ test/t1.py
@@ -1,25 +1,2 @@
 #!/usr/bin/python
 #coding:utf-8
-
-d='face'
-class sophia():
-    alvin='Alvin Wan'
-    def tomath(self):
-        print('hahaha')
-
-class ok():
-    def tomath(self):
-        print('hahaha')
-
-if __name__ =='__main__':
-    print("hello")
-
-a=u'你好啊'
-import os
-print(os.getcwd())
-dir=os.path.dirname(__file__)
-name='t1.py'
-
-if os.path.exists(os.path.join(dir,name)):
-    print('hello')
-    print(os.path.join(dir,name))
