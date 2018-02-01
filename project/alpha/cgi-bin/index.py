419ac6e0e71ac4248c8501f7120e295dce557ce7
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-01 17:38:26 +0800

    I changed again.

--- project/alpha/cgi-bin/index.py
+++ project/alpha/cgi-bin/index.py
@@ -1,6 +1,16 @@
 #coding:utf-8
 import alpha_modules.get_weather as get_weather
-import os,time
+import os,time,cgi,hashlib
+indexDick={}
+def md5(alpha):
+    return hashlib.md5(alpha.encode('utf-8')).hexdigest()
+data = cgi.FieldStorage()
+username = data.getvalue("username")
+password = md5(data.getvalue("password"))
+if username == 'alvin' and password == '20a2495a46e8d2aa6600dec33501326f':
+    auth = True
+else:
+    auth = False
 
 weather={}
 weather['weatherStatus']=get_weather.get_status()
@@ -10,9 +20,12 @@ filename=os.path.join(os.path.dirname(__file__),'index.html')
 htmlfile=open(filename,'r',encoding='UTF-8')
 htmlcontent=htmlfile.read()
 htmlfile.close()
-indexDick={}
+
 indexDick['nowTime']=time.strftime('%Y-%m-%d %H:%M:%S')
 indexDick.update(weather)
 print("Content-type:text/html")
 print()
-print(htmlcontent.format_map(indexDick))
+if auth:
+    print(htmlcontent.format_map(indexDick))
+else:
+    print('username or password error')
--- test/t1.py
+++ test/t1.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 #coding:utf-8
 import time
-
+import hashlib
 print(time.strftime('%Y %m-%d %H:%M:%S'))
+password='wankaihao'
+#print(hashlib.md5(password).hexdigest())
+
+def md5(alpha):
+    return hashlib.md5(alpha.encode('utf-8')).hexdigest()
+
+print(md5('sophiroth'))
