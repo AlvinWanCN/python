ed60d6099e563deb4a7c17bd88d6909c5376d626
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-02 13:40:51 +0800

    I changed again.

---
+++ project/alpha/cgi-bin/alpha_modules/alphaDB.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#coding:utf-8
+import pymysql,time
+#db = pymysql.connect('hostname','user','password','databaseName')
+db = pymysql.connect(
+    host='localhost',
+    password='alpha',
+    user='alpha',
+    db='alvinlife',
+    port=3306,
+    charset='utf8'
+)
+cursor=db.cursor()
+class useDB():
+    def insertDB(self,a,b,c,d):
+        inserSql="insert into account set username = %s ,password = '%s',application = '%s',comment='%s'" %(a,b,c,d)
+        try:
+            cursor.execute(inserSql)
+            db.commit()
+            print('commit success')
+        except Exception as e:
+            db.rollback()
+            print('commit failed')
+            print (e)
+        finally:
+            db.close()
+    def queryDB(self,sql):
+        querySql = sql
+        cursor.execute(querySql)
+        data=cursor.fetchall()
+        return (data)
--- project/alpha/cgi-bin/alpha_modules/new_account.py
+++ project/alpha/cgi-bin/alpha_modules/new_account.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 #coding:utf-8
 import cgi
+from alphaDB import useDB
+udb=useDB()
 
 data=cgi.FieldStorage()
 new_account={}
@@ -8,6 +10,11 @@ new_account['app']=data.getvalue('app')
 new_account['username']=data.getvalue('username')
 new_account['password']=data.getvalue('password')
 new_account['comment']=data.getvalue('comment')
+if new_account['app'] and new_account['username'] and new_account['password']:
+    udb.insertDB(new_account['username'],new_account['password'],new_account['app'],new_account['comment'])
+    new_account['result'] ='{app}的{username}账号已存储到数据。'.format_map(new_account)
+else:
+    new_account['result']='缺少关键内容，application,username,password 是必填项。'
 #print(new_account)
 html="""
 <!DOCTYPE html>
@@ -17,11 +24,11 @@ html="""
     <title>添加新账号结果</title>
 </head>
 <body>
-    您已成功添加新账号
+    {result}
 </body>
 </html>
 """
 #print("Content-Type: application/json")
 print("Content-type:text/html")
 print()
-print(html)
+print(html.format_map(new_account))
