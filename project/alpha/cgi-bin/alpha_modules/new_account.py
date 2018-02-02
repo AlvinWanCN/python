c60f0151b3e6dcf2eb599aabe317b2fe90efd235
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-02 13:00:10 +0800

    I changed again.

---
+++ project/alpha/cgi-bin/alpha_modules/new_account.py
@@ -0,0 +1,27 @@
+#/usr/bin/python3
+#coding:utf-8
+import cgi
+
+data=cgi.FieldStorage()
+new_account={}
+new_account['app']=data.getvalue('app')
+new_account['username']=data.getvalue('username')
+new_account['password']=data.getvalue('password')
+new_account['comment']=data.getvalue('comment')
+#print(new_account)
+html="""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>添加新账号结果</title>
+</head>
+<body>
+    您已成功添加新账号
+</body>
+</html>
+"""
+#print("Content-Type: application/json")
+print("Content-type:text/html")
+print()
+print(html)
