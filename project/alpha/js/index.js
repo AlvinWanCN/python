f2ca404abd1c8663712fed6b90070f7e1083a212
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-02 18:11:03 +0800

    I changed again.

--- project/alpha/js/index.js
+++ project/alpha/js/index.js
@@ -1,3 +1,10 @@
 /**
  * Created by Alvin Wan on 2/2/2018.
  */
+document.write("<h1>This is a heading</h1>");
+document.write("<p>This is a paragraph.</p>");
+function sophia()
+{
+x=document.getElementById("nice");  // 找到元素
+x.innerHTML="this p is alvin's world";    // 改变内容
+}
