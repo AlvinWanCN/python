2af23834163df5f64c2073ba7785fedf5f16f5ad
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-06 10:55:07 +0800

    I changed again.

--- project/alpha/html/uploadFile.py
+++ project/alpha/html/uploadFile.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 #conding:utf-8
-import cgi
+import cgi,json
 data=cgi.FieldStorage()
 all=data.getvalue(all())
+text=data.getvalue('text')
+file=data.getvalue('file')
+
+
+#success['dateType']=type(tdate)
+
+
+#lastdata=udb.queryDB()
+#print("Content-type:text/html")
+print("Content-Type: text/html")
+print()
+print('你好')
