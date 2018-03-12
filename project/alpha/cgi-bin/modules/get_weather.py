09c30a7513ea03332cdc0e4e1bb600bf0d2726fc
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-12 10:10:11 +0800

    optimize script.

--- project/alpha/cgi-bin/modules/get_weather.py
+++ project/alpha/cgi-bin/modules/get_weather.py
@@ -8,29 +8,25 @@ url=host + path
 content=urllib.request.urlopen(url).read()
 html=etree.HTML(content)
 def get_status(): ##获取天气状况
-    weather_status=html.xpath('//*[@id="wrap"]/div[4]/div[1]/div[3]/dl[1]/dd/ul/li[1]/span[2]')[0].text
-    if weather_status:
-        return weather_status
-    else:
+    try:
+        weather_status=html.xpath('//*[@id="wrap"]/div[4]/div[1]/div[3]/dl[1]/dd/ul/li[1]/span[2]')[0].text
+        if weather_status:
+            return weather_status
+    except Exception as e:
         return "unknow"
 
 def get_max_temperature(): #获取最高气温
     #weather_max_temperature=re.findall(r'</b>(-?\d)<i>',str(content))[0]
-    weather_max_temperature=html.xpath('//*[@id="wrap"]/div[4]/div[1]/div[3]/dl[1]/dd/ul/li[2]/span/text()')[0]
-    if weather_max_temperature:
-        return weather_max_temperature
-    else:
+    try:
+        weather_max_temperature=html.xpath('//*[@id="wrap"]/div[4]/div[1]/div[3]/dl[1]/dd/ul/li[2]/span/text()')[0]
+        if weather_max_temperature:
+            return weather_max_temperature
+    except Exception as e:
         return "unknow"
 def get_min_temperature():#获取最低气温
-    weather_min__temperature=re.findall(r'</b>(-?\d+)<i>',str(content))[-1]
-    if weather_min__temperature:
-        return weather_min__temperature
-    else:
+    try:
+        weather_min__temperature=re.findall(r'</b>(-?\d+)<i>',str(content))[-1]
+        if weather_min__temperature:
+            return weather_min__temperature
+    except Exception as e:
         return "unknow"
-
-#print (html.xpath('//*[@id="wrap"]/div[4]/div[1]/div[3]/dl[1]/dd/ul/li[2]/span/text()')[0])
-
-print(get_status())
-print(get_max_temperature())
-print(get_min_temperature())
-#print(content)
